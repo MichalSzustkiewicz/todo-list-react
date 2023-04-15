22afa09df4b24812c2ec715168c6376d51dd8be1
Michal Szustkiewicz <szustka755@gmail.com>
2023-04-15 11:35:52 +0200

    adding empty task prevented

--- src/Form/index.js
+++ src/Form/index.js
@@ -7,12 +7,12 @@ const Form = ({ addNewTask }) => {
     const onFormSubmit = (event) => {
         event.preventDefault();
 
-        if (newTaskContent !== "") {
-            addNewTask(newTaskContent.trim());
+        const trimmedNewTaskContent = newTaskContent.trim();
+
+        if (trimmedNewTaskContent !== "") {
+            addNewTask(trimmedNewTaskContent);
             setNewTaskContent("");
         }
-
-        return;
     };
 
     return (
